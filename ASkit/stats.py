4c9e2d1d4b0fa4aa90ca642c56aa1641e53880ec
jzluo <20971593+jzluo@users.noreply.github.com>
2022-06-06 20:07:27 -0400

    separate nr iterations and half-stepping

--- ASkit/stats.py
+++ ASkit/stats.py
@@ -38,7 +38,7 @@ def _get_XW(X, pi):
 
 
 @jax.jit
-def _newton_iteration(X, y, b, step_limit):
+def _newton_iteration(X, y, b, max_halfstep):
     pi = _predict(X, b)
     XW = _get_XW(X, pi)
     FIM = _fisher_info_mtx(XW)
@@ -54,14 +54,16 @@ def _newton_iteration(X, y, b, step_limit):
         "old_ll": ll,
         "new_ll": new_ll,
         "step": 0,
-        "step_limit": step_limit,
+        "max_halfstep": max_halfstep,
+        "X": X,
+        "y": y,
     }
     res = jax.lax.while_loop(_step_condition, _step_halving, carry)
     return res
 
 
 def _step_condition(carry):
-    break_condition = carry["step"] == carry["step_limit"]
+    break_condition = carry["step"] == carry["max_halfstep"]
     return ~break_condition & (carry["new_ll"] > carry["old_ll"])
 
 
@@ -76,42 +78,44 @@ def _step_halving(carry):
 def _newton_cond(carry):
     return (
         jnp.linalg.norm(carry["b_new"] - carry["b"]) > carry["convergence_limit"]
-    ) & (carry["step"] < carry["step_limit"])
+    ) & (carry["iter"] < carry["max_iter"])
 
 
 def _newton_raphson(carry):
     X, y = carry["X"], carry["y"]
     carry["b"] = carry["b_new"]
-    carry["b_new"] = _newton_iteration(X, y, carry["b_new"], carry["step_limit"])[
-        "b_new"
-    ]
-    carry["step"] += 1
+    iter = _newton_iteration(X, y, carry["b_new"], carry["max_halfstep"])
+    carry["new_ll"] = iter["new_ll"]
+    carry["b_new"] = iter["b_new"]
+    carry["iter"] += 1
     return carry
 
 
 @jax.jit
-def fit_jax(
+def fit_firth(
     X,
     y,
-    step_limit=1000,
+    max_iter=25,
+    max_halfstep=1000,
     convergence_limit=0.0001,
 ):
     device = "cpu"
     X = jax.device_put(X, jax.devices(device)[0])
     y = jax.device_put(y, jax.devices(device)[0])
     b = jnp.zeros(X.shape[1])
-    b_new = _newton_iteration(X, y, b, step_limit)["b_new"]
+    b_new = _newton_iteration(X, y, b, max_iter)["b_new"]
     carry = {
         "b_new": b_new,
         "b": b,
         "old_ll": 0,
         "new_ll": 0,
-        "step": 0,
-        "step_limit": 1000,
+        "iter": 1,
+        "max_iter": max_iter,
+        "max_halfstep": max_halfstep,
         "convergence_limit": convergence_limit,
         "X": X,
         "y": y,
     }
     res = jax.lax.while_loop(_newton_cond, _newton_raphson, carry)
 
-    return res["step"], res["b_new"], res["new_ll"]
+    return res["iter"], res["b_new"], res["new_ll"]
