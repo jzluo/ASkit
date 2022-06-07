50036f06a614a0faa2ecf7bc4546b058e0831abd
jzluo <20971593+jzluo@users.noreply.github.com>
2022-06-06 20:10:31 -0400

    reorganize

--- ASkit/stats.py
+++ ASkit/stats.py
@@ -5,36 +5,50 @@ from jax.scipy.special import expit
 jax.config.update("jax_enable_x64", True)
 
 
-def _predict(X, b):
-    return expit(jnp.matmul(X, b))
+@jax.jit
+def fit_firth(
+    X,
+    y,
+    max_iter=25,
+    max_halfstep=1000,
+    convergence_limit=0.0001,
+):
+    device = "cpu"
+    X = jax.device_put(X, jax.devices(device)[0])
+    y = jax.device_put(y, jax.devices(device)[0])
+    b = jnp.zeros(X.shape[1])
+    b_new = _newton_iteration(X, y, b, max_iter)["b_new"]
+    carry = {
+        "b_new": b_new,
+        "b": b,
+        "old_ll": 0,
+        "new_ll": 0,
+        "iter": 1,
+        "max_iter": max_iter,
+        "max_halfstep": max_halfstep,
+        "convergence_limit": convergence_limit,
+        "X": X,
+        "y": y,
+    }
+    res = jax.lax.while_loop(_newton_cond, _newton_raphson, carry)
+
+    return res["iter"], res["b_new"], res["new_ll"]
 
 
-def _firth_loglikelihood(X, y, b):
-    pi = _predict(X, b)
-    XW = _get_XW(X, pi)
-    FIM = _fisher_info_mtx(XW)
-    penalty = 0.5 * jnp.log(jnp.linalg.det(FIM))
-
-    return -1 * (jnp.sum(y * jnp.log(pi) + (1 - y) * jnp.log(1 - pi)) + penalty)
-
-
-def _fisher_info_mtx(XW):
-    return jnp.matmul(XW.T, XW)
+def _newton_raphson(carry):
+    X, y = carry["X"], carry["y"]
+    carry["b"] = carry["b_new"]
+    iter = _newton_iteration(X, y, carry["b_new"], carry["max_halfstep"])
+    carry["new_ll"] = iter["new_ll"]
+    carry["b_new"] = iter["b_new"]
+    carry["iter"] += 1
+    return carry
 
 
-def _hat_diag(XW):
-    Q = jnp.linalg.qr(XW, mode="reduced")[0]
-    hat = jnp.einsum("ij,ij->i", Q, Q, precision="highest")
-    return hat
-
-
-def _get_XW(X, pi):
-    rootW = jnp.sqrt(jnp.multiply(pi, 1 - pi))
-    XW = rootW[:, jnp.newaxis] * X
-    return XW
-
-
-###################################################################################
+def _newton_cond(carry):
+    return (
+        jnp.linalg.norm(carry["b_new"] - carry["b"]) > carry["convergence_limit"]
+    ) & (carry["iter"] < carry["max_iter"])
 
 
 @jax.jit
@@ -75,47 +89,30 @@ def _step_halving(carry):
     return carry
 
 
-def _newton_cond(carry):
-    return (
-        jnp.linalg.norm(carry["b_new"] - carry["b"]) > carry["convergence_limit"]
-    ) & (carry["iter"] < carry["max_iter"])
+def _predict(X, b):
+    return expit(jnp.matmul(X, b))
 
 
-def _newton_raphson(carry):
-    X, y = carry["X"], carry["y"]
-    carry["b"] = carry["b_new"]
-    iter = _newton_iteration(X, y, carry["b_new"], carry["max_halfstep"])
-    carry["new_ll"] = iter["new_ll"]
-    carry["b_new"] = iter["b_new"]
-    carry["iter"] += 1
-    return carry
+def _firth_loglikelihood(X, y, b):
+    pi = _predict(X, b)
+    XW = _get_XW(X, pi)
+    FIM = _fisher_info_mtx(XW)
+    penalty = 0.5 * jnp.log(jnp.linalg.det(FIM))
+
+    return -1 * (jnp.sum(y * jnp.log(pi) + (1 - y) * jnp.log(1 - pi)) + penalty)
 
 
-@jax.jit
-def fit_firth(
-    X,
-    y,
-    max_iter=25,
-    max_halfstep=1000,
-    convergence_limit=0.0001,
-):
-    device = "cpu"
-    X = jax.device_put(X, jax.devices(device)[0])
-    y = jax.device_put(y, jax.devices(device)[0])
-    b = jnp.zeros(X.shape[1])
-    b_new = _newton_iteration(X, y, b, max_iter)["b_new"]
-    carry = {
-        "b_new": b_new,
-        "b": b,
-        "old_ll": 0,
-        "new_ll": 0,
-        "iter": 1,
-        "max_iter": max_iter,
-        "max_halfstep": max_halfstep,
-        "convergence_limit": convergence_limit,
-        "X": X,
-        "y": y,
-    }
-    res = jax.lax.while_loop(_newton_cond, _newton_raphson, carry)
+def _fisher_info_mtx(XW):
+    return jnp.matmul(XW.T, XW)
 
-    return res["iter"], res["b_new"], res["new_ll"]
+
+def _hat_diag(XW):
+    Q = jnp.linalg.qr(XW, mode="reduced")[0]
+    hat = jnp.einsum("ij,ij->i", Q, Q, precision="highest")
+    return hat
+
+
+def _get_XW(X, pi):
+    rootW = jnp.sqrt(jnp.multiply(pi, 1 - pi))
+    XW = rootW[:, jnp.newaxis] * X
+    return XW
