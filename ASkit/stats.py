79b3a1c02330c52a0044d48ecf7ca18c8b671988
jzluo <20971593+jzluo@users.noreply.github.com>
2022-06-06 23:17:18 -0400

    janky masking of samples with missing phenos to keep dimensions consistent for jit/vmap

--- ASkit/stats.py
+++ ASkit/stats.py
@@ -14,55 +14,62 @@ def fit_firth(
     convergence_limit=0.0001,
 ):
     device = "cpu"
+    mask = y.copy()
     X = jax.device_put(X, jax.devices(device)[0])
     y = jax.device_put(y, jax.devices(device)[0])
     b = jnp.zeros(X.shape[1])
-    b_new = _newton_iteration(X, y, b, max_iter)["b_new"]
-    carry = {
+    b_new = _newton_iteration(X, y, b, max_iter, mask)["b_new"]
+    nr_carry = {
         "b_new": b_new,
         "b": b,
-        "old_ll": 0,
-        "new_ll": 0,
+        "ll": 0,
         "iter": 1,
         "max_iter": max_iter,
         "max_halfstep": max_halfstep,
         "convergence_limit": convergence_limit,
         "X": X,
         "y": y,
+        "mask": mask,
     }
-    res = jax.lax.while_loop(_newton_cond, _newton_raphson, carry)
+    res = jax.lax.while_loop(_newton_cond, _newton_raphson, nr_carry)
 
-    return res["iter"], res["b_new"], res["new_ll"]
-
-
-def _newton_raphson(carry):
-    X, y = carry["X"], carry["y"]
-    carry["b"] = carry["b_new"]
-    iter = _newton_iteration(X, y, carry["b_new"], carry["max_halfstep"])
-    carry["new_ll"] = iter["new_ll"]
-    carry["b_new"] = iter["b_new"]
-    carry["iter"] += 1
-    return carry
+    return res["iter"], res["b_new"], res["ll"]
 
 
-def _newton_cond(carry):
-    return (
-        jnp.linalg.norm(carry["b_new"] - carry["b"]) > carry["convergence_limit"]
-    ) & (carry["iter"] < carry["max_iter"])
+def _newton_raphson(nr_carry):
+    X, y = nr_carry["X"], nr_carry["y"]
+    nr_carry["b"] = nr_carry["b_new"]
+    iter = _newton_iteration(
+        X, y, nr_carry["b_new"], nr_carry["max_halfstep"], nr_carry["mask"]
+    )
+    nr_carry["ll"] = iter["new_ll"]
+    nr_carry["b_new"] = iter["b_new"]
+    nr_carry["iter"] += 1
+    return nr_carry
+
+
+def _newton_cond(nr_carry):
+    iter_limit = nr_carry["iter"] == nr_carry["max_iter"]
+    converged = (
+        jnp.linalg.norm(nr_carry["b_new"] - nr_carry["b"])
+        < nr_carry["convergence_limit"]
+    )
+    return ~iter_limit & ~converged
 
 
 @jax.jit
-def _newton_iteration(X, y, b, max_halfstep):
-    pi = _predict(X, b)
+def _newton_iteration(X, y, b, max_halfstep, mask):
+    pi = _predict(X, b, mask)
     XW = _get_XW(X, pi)
-    FIM = _fisher_info_mtx(XW)
+    FIM = jnp.matmul(XW.T, XW)
     hat = _hat_diag(XW)
-    U = jnp.matmul(X.T, y - pi + jnp.multiply(hat, 0.5 - pi))
-    b_new = b + jnp.linalg.lstsq(FIM, U)[0]
+    U_star = jnp.matmul(X.T, y - pi + jnp.multiply(hat, 0.5 - pi))
+    b_new = b + jnp.linalg.lstsq(FIM, U_star)[0]
+    pi_new = _predict(X, b_new, mask)
+    ll = _firth_loglikelihood(X, y, pi, mask)
+    new_ll = _firth_loglikelihood(X, y, pi_new, mask)
     # step halving
-    ll = _firth_loglikelihood(X, y, b)
-    new_ll = _firth_loglikelihood(X, y, b_new)
-    carry = {
+    step_carry = {
         "b_new": b_new,
         "b": b,
         "old_ll": ll,
@@ -71,39 +78,46 @@ def _newton_iteration(X, y, b, max_halfstep):
         "max_halfstep": max_halfstep,
         "X": X,
         "y": y,
+        "mask": mask,
     }
-    res = jax.lax.while_loop(_step_condition, _step_halving, carry)
+    res = jax.lax.while_loop(_step_condition, _step_halving, step_carry)
     return res
 
 
-def _step_condition(carry):
-    break_condition = carry["step"] == carry["max_halfstep"]
-    return ~break_condition & (carry["new_ll"] > carry["old_ll"])
+def _step_condition(step_carry):
+    step_limit = step_carry["step"] == step_carry["max_halfstep"]
+    return ~step_limit & (step_carry["new_ll"] > step_carry["old_ll"])
 
 
-def _step_halving(carry):
-    X, y = carry["X"], carry["y"]
-    carry["b_new"] = carry["b"] + 0.5 * (carry["b_new"] - carry["b"])
-    carry["new_ll"] = _firth_loglikelihood(X, y, carry["b_new"])
-    carry["step"] += 1
-    return carry
+def _step_halving(step_carry):
+    X, y, mask = step_carry["X"], step_carry["y"], step_carry["mask"]
+    step_carry["b_new"] = step_carry["b"] + 0.5 * (
+        step_carry["b_new"] - step_carry["b"]
+    )
+    pi = _predict(X, step_carry["b_new"], mask)
+    step_carry["new_ll"] = _firth_loglikelihood(X, y, pi, mask)
+    step_carry["step"] += 1
+    return step_carry
 
 
-def _predict(X, b):
-    return expit(jnp.matmul(X, b))
+def _mask_array(array, mask, fill_val):
+    return jnp.where(mask == -9, fill_val, array)
 
 
-def _firth_loglikelihood(X, y, b):
-    pi = _predict(X, b)
+def _predict(X, b, mask):
+    pi = expit(jnp.matmul(X, b))
+    pi = _mask_array(pi, mask, 0)
+    return pi
+
+
+def _firth_loglikelihood(X, y, pi, mask):
     XW = _get_XW(X, pi)
-    FIM = _fisher_info_mtx(XW)
+    FIM = jnp.matmul(XW.T, XW)
     penalty = 0.5 * jnp.log(jnp.linalg.det(FIM))
 
-    return -1 * (jnp.sum(y * jnp.log(pi) + (1 - y) * jnp.log(1 - pi)) + penalty)
-
-
-def _fisher_info_mtx(XW):
-    return jnp.matmul(XW.T, XW)
+    pi0 = _mask_array(pi, mask, 0)
+    pi1 = _mask_array(pi, mask, 1)
+    return -1 * (jnp.sum(y * jnp.log(pi1) + (1 - y) * jnp.log(1 - pi0)) + penalty)
 
 
 def _hat_diag(XW):
